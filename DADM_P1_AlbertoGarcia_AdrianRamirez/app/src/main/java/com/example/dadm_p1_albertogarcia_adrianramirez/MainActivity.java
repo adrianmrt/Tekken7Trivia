061c571ae216b71fc8baa78265e5f07bff354e34
Alpuerro <phonos26@gmail.com>
2021-10-08 22:29:17 +0200

    Imput de nombre
    Ya hay un input de nombre en el main con un aviso por si lo dejas en blanco. Falta saber cómo pasar la información entre actividades.

--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/MainActivity.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/MainActivity.java
@@ -4,21 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout _nickLayout;
+    private EditText _nick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        _nick = findViewById(R.id.nickInput);
+        _nickLayout = findViewById(R.id.nickInputLayout);
     }
 
-    public void pulsarBoton(View v){
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        Intent intent=new Intent(getApplicationContext(),QuestionActivity.class);
-        startActivity(intent);
+        _nickLayout.setErrorEnabled(false);
+    }
 
+    public void play(View v){
+        if(TextUtils.isEmpty(_nick.getText().toString())){
+            _nickLayout.setError("Campo vacío");
+            _nickLayout.setErrorEnabled(true);
+        } else {
+            Bundle bundle = new Bundle();
+            bundle.putString("nick", _nick.getText().toString());
+
+            Intent intent=new Intent(getApplicationContext(),QuestionActivity.class);
+            startActivity(intent);
+        }
     }
 }
