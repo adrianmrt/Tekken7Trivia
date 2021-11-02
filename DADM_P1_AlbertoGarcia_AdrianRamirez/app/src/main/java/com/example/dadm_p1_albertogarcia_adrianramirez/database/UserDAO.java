c25157fd3a11cc947f2f3f3381b3753d81534718
Alpuerro <phonos26@gmail.com>
2021-11-02 21:13:55 +0100

    Base de datos básica

--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/database/UserDAO.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/database/UserDAO.java
@@ -2,9 +2,15 @@ package com.example.dadm_p1_albertogarcia_adrianramirez.database;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface UserDAO {
     @Insert
     public void addUser(User user);
+
+    @Query("select * from users")
+    public List<User> getUsers();
 }
--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/main/MainUserFragment.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/main/MainUserFragment.java
@@ -4,19 +4,29 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dadm_p1_albertogarcia_adrianramirez.R;
 import com.example.dadm_p1_albertogarcia_adrianramirez.database.User;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.List;
 
 public class MainUserFragment extends Fragment {
+
+    TextInputLayout userNameLayout;
+    TextView usersListText;
     EditText userName;
     Button addUser;
+
+    List<User> usersList;
 
     public MainUserFragment() {
         // Required empty public constructor
@@ -32,17 +42,35 @@ public class MainUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_user, container, false);
 
+        userNameLayout = view.findViewById(R.id.addUserNameLayout);
+        usersListText = view.findViewById(R.id.usersListText);
         userName = view.findViewById(R.id.addUserName);
         addUser = view.findViewById(R.id.addUserButton);
 
         addUser.setOnClickListener(v -> {
-            User user = new User();
-            user.setName(userName.getText().toString());
+            String usersListInfo = null;
 
-            MainActivity.userDataBase.userDAO().addUser(user);
-            Toast.makeText(getActivity(), "User added", Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(userName.getText().toString())) {
+                userNameLayout.setError("Campo vacío");
+                userNameLayout.setErrorEnabled(true);
+            } else {
+                User user = new User();
+                user.setName(userName.getText().toString());
 
-            userName.setText("");
+                MainActivity.userDataBase.userDAO().addUser(user);
+                Toast.makeText(getActivity(), "User added", Toast.LENGTH_SHORT).show();
+
+                usersList = MainActivity.userDataBase.userDAO().getUsers();
+
+                for (User users : usersList){
+                    String name = user.getName();
+
+                    usersListInfo = "Nombre: " + name + "\n\n";
+                }
+
+                usersListText.setText(usersListInfo);
+                userName.setText("");
+            }
         });
 
         return view;
