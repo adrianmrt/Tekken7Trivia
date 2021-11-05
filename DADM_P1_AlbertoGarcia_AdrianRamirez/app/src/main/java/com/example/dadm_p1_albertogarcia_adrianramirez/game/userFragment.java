3e827e74a5cb18f6ea19a260e8186a7874df9968
Adrian <72496013+adrianmrt@users.noreply.github.com>
2021-11-05 14:29:20 +0100

    timer Done

--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/game/userFragment.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/game/userFragment.java
@@ -1,11 +1,13 @@
 package com.example.dadm_p1_albertogarcia_adrianramirez.game;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.dadm_p1_albertogarcia_adrianramirez.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class userFragment extends Fragment {
@@ -25,25 +30,35 @@ public class userFragment extends Fragment {
     TextView playerNameView;
     Parcelable[] _questions;
 
+    //timer elements
+    Timer t;
+    boolean finished;
+    TextView timer;
+    int sec, min, mili = 0;
+    String time;
+    Handler handler;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        finished = false;
+        time = "";
         //creation of object that receives data from gameFragment
         getParentFragmentManager().setFragmentResultListener("answerPass", this, (requestKey, bundle) -> {
             boolean result = bundle.getBoolean("answer");
-            if (result){
-                score+=10;
+            if (result) {
+                score += 10;
                 scoreView.setText(score.toString());
             }
         });
 
         getParentFragmentManager().setFragmentResultListener("finished", this, (requestKey, bundle) -> {
+            finished = true;
             Intent intent = new Intent(getActivity(), ScoreActivity.class);
             intent.putExtra("playerName", playerName);
             intent.putExtra("score", score.toString());
-            intent.putExtra("questions",_questions);
+            intent.putExtra("questions", _questions);
+            intent.putExtra("time", time);
             getActivity().startActivity(intent);
         });
     }
@@ -51,8 +66,8 @@ public class userFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Bundle bundle= getArguments();
-        _questions= bundle.getParcelableArray("questions");
+        Bundle bundle = getArguments();
+        _questions = bundle.getParcelableArray("questions");
         return inflater.inflate(R.layout.user_fragment, container, false);
     }
 
@@ -61,7 +76,7 @@ public class userFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         score = requireArguments().getInt("initialScore");
-        playerName=  requireArguments().getString("playerName");
+        playerName = requireArguments().getString("playerName");
 
         //we reference the elements of the layout
         scoreView = view.findViewById(R.id.score);
@@ -69,5 +84,30 @@ public class userFragment extends Fragment {
 
         scoreView.setText(score.toString());
         playerNameView.setText(playerName);
+
+        timer = view.findViewById(R.id.timer);
+        time = Integer.toString(min) + "min " + Integer.toString(sec) + "s";
+        timer.setText(time);
+
+        t = new Timer();
+        t.scheduleAtFixedRate(
+                new TimerTask() {
+                    public void run() {
+                        startTimer();
+                    }
+                },
+                0,      // run first occurrence immediatetly
+                1000); // run every x seconds
+    }
+
+
+    public void startTimer() {
+        time = Integer.toString(min) + "min " + Integer.toString(sec) + "s";
+        timer.setText(time);
+        sec++;
+        if (sec == 60) {
+            min++;
+            sec = 0;
+        }
     }
 }
