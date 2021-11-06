4d737836854f493f695f017d0fa5d62bed762a01
Alpuerro <phonos26@gmail.com>
2021-11-06 15:58:06 +0100

    Merge branch 'main' into Alberto

--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/database/DatabaseViewModel.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/database/DatabaseViewModel.java
@@ -12,15 +12,22 @@ public class  DatabaseViewModel extends AndroidViewModel {
 
     LiveData<List<Question>> allQuestions;
     LiveData<List<User>> allUsers;
+    LiveData<List<RankingUnit>> ranking;
+
     private QuestionRepository questionRepository;
     private UserRepository userRepository;
+    private RankingRepository rankingRepository;
 
     public DatabaseViewModel(@NonNull Application application) {
         super(application);
         questionRepository= new QuestionRepository(application);
         userRepository= new UserRepository(application);
+        rankingRepository= new RankingRepository(application);
+
         allQuestions= questionRepository.getQuestions();
         allUsers= userRepository.GetUsers();
+        ranking= rankingRepository.getRanking();
+
     }
 
     public QuestionRepository getQuestionRepository() {
@@ -29,12 +36,15 @@ public class  DatabaseViewModel extends AndroidViewModel {
 
     public void InsertQuestion(Question question){ questionRepository.InsertQuestion(question);}
     public void InsertUser(User user){ userRepository.InsertUser(user);}
+    public void InsertRanking(RankingUnit rankingUnit){ rankingRepository.InsertRanking(rankingUnit);}
 
     public LiveData<List<Question>> getAllQuestions() {
         return allQuestions;
     }
-
     public LiveData<List<User>> getAllUsers() {
         return allUsers;
+    }
+    public LiveData<List<RankingUnit>> getAllRanking() {
+        return ranking;
     }
 }
