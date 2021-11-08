abd40ccae38ace7df45b7290565fee3b69d32eb8
Alpuerro <phonos26@gmail.com>
2021-11-08 11:17:08 +0100

    otro main update

--- DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/game/AnswerFragment.java
+++ DADM_P1_AlbertoGarcia_AdrianRamirez/app/src/main/java/com/example/dadm_p1_albertogarcia_adrianramirez/game/AnswerFragment.java
@@ -16,7 +16,7 @@ import com.example.dadm_p1_albertogarcia_adrianramirez.R;
 import com.example.dadm_p1_albertogarcia_adrianramirez.database.DatabaseViewModel;
 import com.example.dadm_p1_albertogarcia_adrianramirez.database.Question;
 
-public class AnswerFragment extends Fragment {
+public class answerFragment extends Fragment {
 
     int type;
     View rootView;
